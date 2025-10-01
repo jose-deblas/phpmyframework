617745a443d016b83e46281c9df9675b47574e86
jose-deblas <jose.de.blas.navas@gmail.com>
2025-10-01 15:34:35 +0200

    feat: dependency injection config providers

--- config/providers.php
+++ config/providers.php
@@ -4,23 +4,3 @@ return [
     '\App\Providers\ControllerProvider',
     '\App\Providers\BlogProvider'
 ];
-
-/*
-$factories = [
-    'user_repository' => function($container) {
-        return new \App\Repository\UserRepository($container->get('database_connection'));
-    },
-];
-
-$singletons = [
-    'config' => function() {
-        return new \App\Config(['setting1' => 'value1', 'setting2' => 'value2']);
-    },
-];
-
-return [
-    'services' => $services,
-    'factories' => $factories,
-    'singletons' => $singletons,
-];
-*/
